c6cfb8926fc26b2624b896be6e439511165ab29d
mdvanes <es@mdworld.nl>
2018-06-14 13:13:40 +0200

    Add toggle

--- background.js
+++ background.js
@@ -8,16 +8,20 @@ chrome.runtime.onInstalled.addListener(function () {
   chrome.storage.sync.set({ color: '#3aa757' }, function () {
     console.log("The color is green.");
   });
+  chrome.storage.sync.set({ isActive: false }, function () {
+    console.log("Onload isActive is false");
+  });
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     chrome.declarativeContent.onPageChanged.addRules([
       {
         conditions: [
           new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { hostEquals: 'developer.chrome.com' },
+            // pageUrl: { hostEquals: 'developer.chrome.com' },
+            pageUrl: { schemes: ['https', 'file', 'chrome'] }, // TODO debug, should only be https
           })
         ],
         actions: [new chrome.declarativeContent.ShowPageAction()]
-        }
+      }
     ]);
   });
 });
--- popup.js
+++ popup.js
@@ -1,4 +1,6 @@
 let changeColor = document.getElementById('changeColor');
+let activeToggle = document.getElementById('activeToggle');
+let xInterval;
 
 chrome.storage.sync.get('color', function(data) {
   changeColor.style.backgroundColor = data.color;
@@ -13,3 +15,31 @@ changeColor.onclick = function(element) {
           {code: 'document.body.style.backgroundColor = "' + color + '";'});
     });
   };
+
+chrome.storage.sync.get('isActive', function(data) {
+    chrome.extension.getBackgroundPage().console.log('get isactive', data);
+    if(data.isActive) {
+        activeToggle.setAttribute('checked', true);
+    } else {
+        activeToggle.removeAttribute('checked');
+    }
+});
+
+activeToggle.onclick = function(event) {
+  chrome.storage.sync.set({ isActive: event.target.checked }, function () {
+    chrome.extension.getBackgroundPage().console.log("click isActive is ", event.target.checked);
+    if(event.target.checked) {
+      // TODO interval breaks when popup is closed. Move to chrome.extension.getBackgroundPage().setInterval and supply tabs[0].id or something
+      xInterval = setInterval(() => {
+        chrome.extension.getBackgroundPage().console.log("Interval!");
+      }, 2000)
+    } else {
+      clearInterval(xInterval);
+    }
+  });
+}
+
+// TODO is active on?
+// TODO is this the target page?
+// TODO does it work when the target page is not the active tab?
+// TODO periodically log check
