14505f1225bd2a0e0848512325343d253fe911af
M.D. van Es <es@mdworld.nl>
2018-06-16 13:10:37 +0200

    Reload and click interval

--- popup.js
+++ popup.js
@@ -1,6 +1,5 @@
 let changeColor = document.getElementById('changeColor');
 let activeToggle = document.getElementById('activeToggle');
-//let xInterval;
 
 chrome.storage.sync.get('color', function(data) {
   changeColor.style.backgroundColor = data.color;
@@ -27,7 +26,6 @@ chrome.storage.sync.get('isActive', function(data) {
 
 activeToggle.onclick = function(event) {
   chrome.storage.sync.set({ isActive: event.target.checked }, function () {
-    chrome.extension.getBackgroundPage().console.log("click isActive is ", event.target.checked);
     if(event.target.checked) {
       chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.extension.getBackgroundPage().setIntervalOn(tabs);
