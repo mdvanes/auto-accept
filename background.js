79755f2ad7537d6106a8d9b089968f2e8361c2d2
M.D. van Es <es@mdworld.nl>
2018-06-16 13:05:02 +0200

    Reload and click interval

--- background.js
+++ background.js
@@ -1,7 +1,3 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function () {
@@ -17,7 +13,8 @@ chrome.runtime.onInstalled.addListener(function () {
         conditions: [
           new chrome.declarativeContent.PageStateMatcher({
             // pageUrl: { hostEquals: 'developer.chrome.com' },
-            pageUrl: { schemes: ['https', 'file', 'chrome'] }, // TODO debug, should only be https
+            // Note: also set in manifest.json / permissions
+            pageUrl: { schemes: ['https', 'file'] }, // TODO debug, should only be https
           })
         ],
         actions: [new chrome.declarativeContent.ShowPageAction()]
@@ -25,3 +22,71 @@ chrome.runtime.onInstalled.addListener(function () {
     ]);
   });
 });
+
+let aaInterval;
+
+// tab id is unique for browser session, tabs[0] is the initiating tab
+function setIntervalOn(tabs) {
+  console.log('%csetIntervalOn', 'color: green;', tabs); // TODO remove
+  const targetTabId = tabs[0].id;
+  //chrome.storage.sync.set({ targetTabId });
+
+  // Only run on tab where toggle was set to true, but can be turned off anywhere
+  aaInterval = setInterval(() => {
+    // Reload tab
+    chrome.tabs.reload(targetTabId, {bypassCache: true}, () => {
+      chrome.extension.getBackgroundPage().console.log('Interval!', targetTabId);
+      // Note: timeout needed to run executeScript in this callback
+      // Click the element
+      setTimeout(() => {
+        chrome.tabs.executeScript(
+          targetTabId,
+          // TODO button selector in options
+          {code: 'document.querySelector(\'button\').click();'}, () => {
+            chrome.extension.getBackgroundPage().console.log('?', targetTabId);
+          });
+      }, 100);
+    });
+    // chrome.tabs.executeScript(
+    //   tabs[0].id,
+    //   {code: 'location.reload();'});
+    // // Click the element
+    // chrome.tabs.executeScript(
+    //   tabs[0].id,
+    //   // TODO button selector in options
+    //   {code: 'document.querySelector(\'button\').click();'});
+    // chrome.extension.getBackgroundPage().console.log("Interval!");
+  }, 2000); // TODO interval in options
+}
+
+// chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab) {
+//   chrome.storage.sync.get('targetTabId', targetTabId => {
+//     if(tabId === targetTabId) {
+//       chrome.extension.getBackgroundPage().console.log('tab updated', targetTabId);
+//       chrome.tabs.executeScript(
+//         targetTabId,
+//         // TODO button selector in options
+//         {code: 'document.querySelector(\'button\').click();'});
+//     }
+//   });
+//   // if(tabId === 87) {
+//   //   chrome.extension.getBackgroundPage().console.log('tab updated');
+//   //   chrome.tabs.executeScript(
+//   //     87,
+//   //     // TODO button selector in options
+//   //     {code: 'document.querySelector(\'button\').click();'});
+//   // }
+//   // if (tab.url.indexOf("http://translate.google.hu/") > -1 &&
+//   //   changeInfo.url === undefined){
+//   //   chrome.tabs.executeScript(tabId, {file: "program.js"} );
+//   // }
+// });
+
+// TODO page match in options
+// TODO only run for  supplied tab
+
+function setIntervalOff() {
+  console.log('setIntervalOff')
+
+  clearInterval(aaInterval);
+}
