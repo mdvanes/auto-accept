05555bec02324517dff592de7c56c1f6ec491419
M.D. van Es <4253562+mdvanes@users.noreply.github.com>
2018-06-16 13:05:02 +0200

    Reload and click interval

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 let changeColor = document.getElementById('changeColor');
 let activeToggle = document.getElementById('activeToggle');
-let xInterval;
+//let xInterval;
 
 chrome.storage.sync.get('color', function(data) {
   changeColor.style.backgroundColor = data.color;
@@ -29,17 +29,15 @@ activeToggle.onclick = function(event) {
   chrome.storage.sync.set({ isActive: event.target.checked }, function () {
     chrome.extension.getBackgroundPage().console.log("click isActive is ", event.target.checked);
     if(event.target.checked) {
-      // TODO interval breaks when popup is closed. Move to chrome.extension.getBackgroundPage().setInterval and supply tabs[0].id or something
-      xInterval = setInterval(() => {
-        chrome.extension.getBackgroundPage().console.log("Interval!");
-      }, 2000)
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.extension.getBackgroundPage().setIntervalOn(tabs);
+      });
     } else {
-      clearInterval(xInterval);
+      chrome.extension.getBackgroundPage().setIntervalOff();
     }
   });
-}
+};
 
-// TODO is active on?
 // TODO is this the target page?
 // TODO does it work when the target page is not the active tab?
 // TODO periodically log check
