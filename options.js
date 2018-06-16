ee34ceb788565be32ab8d5430b6dee8db0d76e6c
M.D. van Es <es@mdworld.nl>
2018-06-16 14:09:35 +0200

    Show properties on options panel

--- options.js
+++ options.js
@@ -1,4 +1,5 @@
 let page = document.getElementById('buttonDiv');
+let activeToggle = document.getElementById('activeToggle');
 const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
 function constructOptions(kButtonColors) {
   for (let item of kButtonColors) {
@@ -12,9 +13,45 @@ function constructOptions(kButtonColors) {
     page.appendChild(button);
   }
 
-  chrome.storage.sync.get('acceptCounter', data => {
-    console.log(data);
+  chrome.storage.sync.get(['acceptCounter', 'reloadTimer', 'isActive'], data => {
     document.getElementById('counter').innerText = data.acceptCounter;
+    document.getElementById('reloadTimer').value = data.reloadTimer;
+    if(data.isActive) {
+      activeToggle.setAttribute('checked', true);
+    } else {
+      activeToggle.removeAttribute('checked');
+    }
   });
+
+  // TODO deduplicate code with popup.js
+  activeToggle.onclick = event => {
+    chrome.storage.sync.set({ isActive: event.target.checked }, () => {
+      if(event.target.checked) {
+        alert('Can only be enabled on target tab by top menu button');
+        // chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+        //   chrome.extension.getBackgroundPage().setIntervalOn(tabs);
+        // });
+      } else {
+        chrome.extension.getBackgroundPage().setIntervalOff();
+      }
+    });
+  };
+
+  chrome.storage.onChanged.addListener(changes => {
+    console.log(changes);
+    if(changes.acceptCounter) {
+      document.getElementById('counter').innerText = changes.acceptCounter.newValue;
+    }
+    if(changes.reloadTimer) {
+      document.getElementById('reloadTimer').value = changes.reloadTimer.newValue;
+    }
+    if(changes.isActive) {
+      if(changes.isActive.newValue) {
+        activeToggle.setAttribute('checked', true);
+      } else {
+        activeToggle.removeAttribute('checked');
+      }
+    }
+  })
 }
 constructOptions(kButtonColors);
