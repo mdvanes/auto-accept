741637cd6920bbc79d40966958a126543909ff60
mdvanes <es@mdworld.nl>
2018-06-21 09:17:24 +0200

    Unravel callbacks

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// addListener is excluded from chromeExtensionAsync, see https://github.com/KeithHenry/chromeExtensionAsync#event-listener-api
 chrome.runtime.onInstalled.addListener(function () {
   chrome.storage.sync.set({
     isActive: false,
@@ -29,54 +30,51 @@ function isOkProtocol(url, protocols) {
 const acceptedProtocols = ['http', 'https', 'file'];
 let aaInterval;
 
-function checkPageAndClick(targetTabId, data) {
-  // Check if the element is available that indicates that this is the configured page
-  chrome.tabs.executeScript(
-    targetTabId,
-    {
+async function checkPageAndClick(targetTabId, data) {
+  try {
+    // Check if the element is available that indicates that this is the configured page
+    const pageSelectorData = await chrome.tabs.executeScript(targetTabId, {
       // TODO susceptible to XSS
       code: `document.querySelector('${data.pageSelector}').innerText;`
-    }, pageSelectorData => {
-      // Only click if the configured page is visible
-      if(pageSelectorData[0] === data.pageValue) {
-        // Note: timeout needed to run executeScript in this callback
-        setTimeout(() => {
-          console.log('Accept!');
-          // Click the element
-          chrome.tabs.executeScript(
-            targetTabId,
-            {
-              // TODO susceptible to XSS
-              code: `document.querySelector('${data.buttonSelector}').click();`
-            }, () => {
-              chrome.storage.sync.get('acceptCounter', acceptCounterData => {
-                const acceptCounter = acceptCounterData.acceptCounter + 1;
-                chrome.storage.sync.set({ acceptCounter });
-                chrome.browserAction.setBadgeText({text: `${acceptCounter}`});
-              });
-              // Because of service worker, it might be needed to submit one more time after succesful submit
-              setTimeout(() => {
-
-                chrome.tabs.executeScript(
-                  targetTabId,
-                  {
-                    // TODO susceptible to XSS
-                    code: `document.querySelector('${data.buttonSelector}').click();`
-                  }, () => {
-                  });
-
-              }, 3000);
-            });
-        }, 100);
-      } else {
-        console.log('Not on configured page');
-      }
     });
+
+    // Only click if the configured page is visible
+    if(pageSelectorData[0] === data.pageValue) {
+      // Note: timeout needed to run executeScript in this callback
+      // TODO async timeout
+      setTimeout(async () => {
+        console.log('Accept!');
+
+        // Click the element
+        await chrome.tabs.executeScript(targetTabId, {
+          // TODO susceptible to XSS
+          code: `document.querySelector('${data.buttonSelector}').click();`
+        });
+
+        // Update the counter state and badge
+        const acceptCounterData = await chrome.storage.sync.get('acceptCounter');
+        const acceptCounter = acceptCounterData.acceptCounter + 1;
+        await chrome.storage.sync.set({ acceptCounter });
+        await chrome.browserAction.setBadgeText({text: `${acceptCounter}`});
+
+        // Because of service worker, it might be needed to submit one more time after succesful submit
+        setTimeout(async () => {
+          await chrome.tabs.executeScript( targetTabId, {
+            // TODO susceptible to XSS
+            code: `document.querySelector('${data.buttonSelector}').click();`
+          });
+        }, 3000); // Wait for page to load after submit
+      }, 100);
+    } else {
+      console.log('Not on configured page');
+    }
+  } catch(err) {
+    console.error(err);
+  }
 }
 
-// TODO unravel Pyramid of Doom
 // tab id is unique for browser session, tabs[0] is the initiating tab
-function setIntervalOn(tabs) {
+async function setIntervalOn(tabs) {
   console.log('%csetIntervalOn', 'color: green; font-weight: bold;', 'tabs:', tabs);
   const targetTabId = tabs[0].id;
 
@@ -85,11 +83,13 @@ function setIntervalOn(tabs) {
     return false;
   }
 
-  chrome.storage.sync.get([
+  try {
+    // Do not destructure, but pass to checkPageAndClick
+    const data = await chrome.storage.sync.get([
     'reloadTimer',
     'buttonSelector',
     'pageSelector',
-    'pageValue'], data => {
+        'pageValue']);
 
     if(!data.buttonSelector || data.buttonSelector === '') {
       alert('First set button selector in options');
@@ -104,21 +104,23 @@ function setIntervalOn(tabs) {
       return false;
     }
 
-    chrome.browserAction.setBadgeBackgroundColor({ color: [15, 160, 59, 255] });
-    chrome.browserAction.setBadgeText({text: '0'});
-    chrome.storage.sync.set({ acceptCounter: 0 });
+    await chrome.browserAction.setBadgeBackgroundColor({ color: [15, 160, 59, 255] });
+    await chrome.browserAction.setBadgeText({text: '0'});
+    await chrome.storage.sync.set({ acceptCounter: 0 });
 
     // Run once before interval, in case the target page is showing when the user enables the plugin
-    checkPageAndClick(targetTabId, data);
+    await checkPageAndClick(targetTabId, data);
 
     // Only run on tab where toggle was set to true, but can be turned off anywhere
-    aaInterval = setInterval(() => {
+    aaInterval = setInterval(async () => {
       // First reload tab to see if the configured page is returned
-      chrome.tabs.reload(targetTabId, {bypassCache: true}, () => {
-          checkPageAndClick(targetTabId, data);
-        });
+      await chrome.tabs.reload(targetTabId, {bypassCache: true});
+      await checkPageAndClick(targetTabId, data);
     }, data.reloadTimer);
-  });
+
+  } catch(err) {
+    console.error(err);
+  }
 }
 
 //   // if (tab.url.indexOf("http://translate.google.hu/") > -1 &&
